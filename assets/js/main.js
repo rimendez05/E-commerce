7114861b83294ed600059d70fcc43493fb23bd44
Ricardo Méndez <rjmenzaval@gmail.com>
2023-03-17 11:09:04 -0500

    E-commerce V1.

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+import loader from './components/loader.js';
+import cart from './components/cart.js';
+import products from './components/products.js';
+import showCart from './components/showCart.js';
+import showMenu from './components/showMenu.js';
+import getProducts from './helpers/getProducts.js';
+
+/*UI elements*/
+/* Ocultar loader */
+loader()
+
+/* Mostrar Menu */
+showMenu()
+
+/* Mostrar Carrito */
+showCart()
+
+/*End UI Elements*/
+
+/* Products */
+const {db, printProducts} = products(await getProducts())
+
+/* Carrito */
+cart(db, printProducts)
+
+
+
